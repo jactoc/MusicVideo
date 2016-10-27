00d2acdf7ba966b2d5ca2ebd0ada110af5a6884c
jactoc <jactoc@gmail.com>
2016-10-26 22:28:00 -0400

    Initial Commit

---
+++ MusiVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusiVideo
+//
+//  Created by jactoc on 2016-10-26.
+//  Copyright © 2016 Jacopo Toccacieli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
